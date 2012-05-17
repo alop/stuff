ebe3d75bdb1b63dd712ae13cdf206fc8aee6468a
Abel Lopez <alopgeek@gmail.com>
2012-05-16 23:26:46 -0700

    added openvpn script

--- routes.sh
+++ routes.sh
@@ -9,43 +9,21 @@ fi
 
 WIFIGW="206.117.88.1"
 
+networks=("207.97.227.0/24" "12.208.176.0/24" "173.194.79.0/24" "75.62.56.0/24" "64.143.224.0/24" "75.55.64.0/24" "75.55.68.0/24" "75.55.96.0/24" )
+
 case $1 in
 up)
-# Make imap.gmail.com work
-route add -net 173.194.79.0/24 $WIFIGW
-
-# Send traffic to jump hosts over the wifi too
-# ewr1 network
-route add -net 75.62.56.0/24 $WIFIGW
-
-# iad1 network
-route add -net 64.143.224.0/24 $WIFIGW
-
-#san1/san2 network
-route add -net 75.55.64.0/24 $WIFIGW
-route add -net 75.55.68.0/24 $WIFIGW
-
-#dfw1 network
-route add -net 75.55.96.0/24 $WIFIGW
+for i in ${networks[@]}
+do
+route add -net $i $WIFIGW
+done
 ;;
 
 down)
-# Make imap.gmail.com work
-route delete -net 173.194.79.0/24 $WIFIGW
-
-# Send traffic to jump hosts over the wifi too
-# ewr1 network
-route delete -net 75.62.56.0/24 $WIFIGW
-
-# iad1 network
-route delete -net 64.143.224.0/24 $WIFIGW
-
-#san1/san2 network
-route delete -net 75.55.64.0/24 $WIFIGW
-route delete -net 75.55.68.0/24 $WIFIGW
-
-#dfw1 network
-route delete -net 75.55.96.0/24 $WIFIGW
+for i in ${networks[@]}
+do
+route delete -net $i $WIFIGW
+done
 ;;
 *)
 echo "Usage: $0 [up|down]"
