23d5b0b83d91ee9e2862503c2cddf8080d321cb3
Abel Lopez <abelopez@cisco.com>
2014-07-08 10:42:03 -0700

    Removed hardcoded GW
    also removed SilverLining networks

--- reroute.sh
+++ reroute.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+MY_GW=`netstat -rn -f inet | grep default | grep -v utun | awk '{print $2}'`
 
 for i in `cat reroute | grep -v "#"`
   do
-    sudo route add -net $i 10.0.1.1
+    sudo route add -net $i $MY_GW
 done
 
